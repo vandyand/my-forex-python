248ed0a26d89cec67b133dcb9bdd6ca4267b8c6d
vandyand <vandyand@gmail.com>
2021-09-03 11:54:41 -0400

    first commit

---
+++ defs_example.py
@@ -0,0 +1,12 @@
+API_KEY = "placeholderApiKeyExample"
+ACCOUNT_ID = "placeholderAccoundIdExample"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}',
+    "Content-Type": "application/json",
+}
+
+BUY = 1
+SELL = -1
+NONE = 0
